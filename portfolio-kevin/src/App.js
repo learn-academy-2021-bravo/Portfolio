5a5c19c209689358689c9958f6b4929d7039c9b8
Kevin Burgin <Kevinburgin33@gmail.com>
2023-04-21 21:55:52 -0700

    updated about and contact page

--- portfolio-kevin/src/App.js
+++ portfolio-kevin/src/App.js
@@ -5,7 +5,6 @@ import './style/NavBar.css'
 import './style/About.css'
 import './style/Contact.css'
 import './style/Project.css'
-import Card from './Card'
 import './index.css'
 import "tailwindcss/tailwind.css"
 import Profile from "./assests/Profile.jpg"
@@ -29,39 +28,18 @@ function App() {
         <body>
             <div className="main">
                 <nav className="navbar">
-                    {/* <div className="pl-3 py-2">
-                        <button onClick={() => handleClick()}>
+                        <button className="pl-5 py-5" onClick={() => handleClick()}>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
-                        </button> */}
+                        </button>
                         
-                    <div className= "nav-link  text-xl md:text-2xl lg:text-3xl text-white text-opacity-50"  
+                    <div className={click ? "nav-link text-xl md:text-2xl lg:text-3xl text-white text-opacity-80 " : "hidden"} 
                         >
-                        <motion.a 
-                        initial={{opacity: 0, x: -100}}
-                        animate={{opacity: 1, x: 0 }}
-                        transition={{ type: "spring", duration: 2, delay: .9}}
-                        hover={{scale: 2}}
-                        href="#home" >Home</motion.a>
-                        <motion.a 
-                        initial={{opacity: 0, x: -100}}
-                        animate={{opacity: 1, x: 0 }}
-                        transition={{ type: "spring", duration: 3, delay: .9}}
-                        hover={{scale: 2}}
-                        href="#about">About</motion.a>
-                        <motion.a 
-                        initial={{opacity: 0, x: -100}}
-                        animate={{opacity: 1, x: 0 }}
-                        transition={{ type: "spring", duration: 4, delay: .9}}
-                        hover={{scale: 2}}
-                        href="#project">Projects</motion.a>
-                        <motion.a 
-                        initial={{opacity: 0, x: -100}}
-                        animate={{opacity: 1, x: 0 }}
-                        transition={{ type: "spring", duration: 5, delay: .9}}
-                        hover={{scale: 2}}
-                        href="#contact">Contact</motion.a>
+                        <a href="#home" >Home</a>
+                        <a href="#about">About</a>
+                        <a href="#project">Projects</a>
+                        <a href="#contact">Contact</a>
                     </div>
                     </nav>
                     <div className="git-link flex flex-end text-white text-opacity-50">
@@ -70,39 +48,67 @@ function App() {
                     <h3>Kevin Burgin</h3>
                 </div>
                 <section id="home">
-                    <motion.div className="intro "
+                    <motion.h1 className="home-title"
                     initial={{opacity: 0}}
                     animate={{opacity: 1}}
-                    transition={{delay:2, duration: 3}}
-                    >Hello, Im Kevin Burgin, Web Developer</motion.div>
-                {/* <div className="intro"> */}
-                    {/* <span className=" ">A Full Stack Developer</span> */}
-                {/* </div> */}
+                    transition={{delay:0, duration: 6}}
+                    >HELLO WORLD</motion.h1>
+                    <motion.div className="home-subtitle" initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{delay:0, duration: 6}}>
+                    Full Stack Software Devleoper
+                    </motion.div>
+                    <motion.div className="home-name" initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{delay:0, duration: 6}}>Kevin Burgin</motion.div>
                 </section>
                 
                 <section id="about">
                 <div className="shadow"></div>
-                    <img className=" profile" src={Profile}></img>
-                    <p>Hello, I'm Kevin a full stack 
-                        <br/>developer based in San Diego</p>
-                    
-                    {/* <p>I am passionate</p> */}
-                    
+                <div className="about-title">About</div>
+               <div className="about-container">
+                
+                <div className='about-img'>
+                    <img className="profile" src={Profile}></img>
+                    </div>
+                    <div className= "bio">As a full-stack software developer with over a year of experience, I have a solid foundation in front-end and back-end development, along with the ability to integrate and manage various databases, APIs, and web services. I am proficient in popular programming languages such as Javascript, Ruby, HTML, CSS, and C#, and have hands-on experience with frameworks such as React, Ruby on Rails, ASP.NET, and .NET. With exceptional problem-solving skills, keen attention to detail, and a strong work ethic, I am confident in my ability to deliver high-quality software solutions that meet and exceed client expectations. </div>
+                    </div>
+
+                    <div className='skill-container'>
+                    {/* <FontAwesomeIcon icon="fa-brands fa-react" /> */}
+                    <div className='skill'>React</div>
+                    <div className='skill'>JavaScript</div>
+                    <div className='skill'>CSS</div>
+                    <div className='skill'>Ruby</div>
+                    </div>
                 </section>
                 <section id="project">
-                    <h1>Projects</h1>
-                        <Card/> 
-                        <Card/>
-                        <Card/>
-                        
+                    <h1 className='project-title'>Projects</h1>
                 </section>
-                <section id="contact" className="">
-                    <h1>Contact</h1>
-                    <p>aksdjdfs</p>
+                <section id="contact" >
+                    <h1 className=" contact text-4xl font-bold">
+                        
+                        <div className='contact-title'>Contact</div>
+                        {/* <div className='contact-personal-email'>Kevinburgin33@gmail.com</div> */}
+                        <div className='contact-card'>
+                            <div className='contact-left'>
+                                <div className='contact-name'>Full Name</div>
+                                    <input className='contact-name-input'></input>
+                                <div className='contact-email'>Email</div>
+                                    <input className='contact-email-input'></input>
+                            </div>
+                            <div className='contact-right'>
+                                <div className='contact-message'>Message</div>
+                                    <input className='contact-message-input'></input>
+                                    
+                                </div>
+                            </div>
+                            <button className='contact-button'>Submit</button>
+                    </h1>
                 </section>
             </div>
-            <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
         </body>       
         </>
     )
